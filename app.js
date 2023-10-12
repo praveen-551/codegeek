b316685c8c4c77f244eebebcb04ce23636d8c8b8
praveen.pattipati@clarivate.com <praveen.pattipati@clarivate.com>
2023-10-12 11:13:49 +0530

    changed async

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-async function asynFunction(){
-    return "async1";
+async function asynFunction() {
+  return "async1";
 }
 
-async function asynFunction1(){
-    return "async2"
+async function asynFunction1() {
+  return "async3";
 }
-asynFunction().then(res=>console.log(res));
-asynFunction1().then(res=>console.log(res));
+asynFunction().then((res) => console.log(res));
+asynFunction1().then((res) => console.log(res));
 
 // async with arrow syntax
-let firstAsyncFunc=async()=>{
-   const result= await (asynFunction());
-   console.log(result);
-}
+let firstAsyncFunc = async () => {
+  const result = await asynFunction();
+  console.log(result);
+};
 firstAsyncFunc();
 
 // async function using await
-async function getAll(){
-    const [result1, result2] = await Promise.all([asynFunction(),asynFunction1()]);
-    console.log(result1+" "+result2);
+async function getAll() {
+  const [result1, result2] = await Promise.all([
+    asynFunction(),
+    asynFunction1(),
+  ]);
+  console.log(result1 + " " + result2);
 }
 getAll();
 
-async function asyncF1(){
-    const res= await asynFunction();
-    return res;
+async function asyncF1() {
+  const res = await asynFunction();
+  return res;
 }
 
 // calling using then
 
-asyncF1().then(res=>{
-    console.log('----------res ------------',res)
-})
+asyncF1().then((res) => {
+  console.log("----------res ------------", res);
+});
